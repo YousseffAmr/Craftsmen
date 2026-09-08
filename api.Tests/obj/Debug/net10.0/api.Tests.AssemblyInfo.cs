8cc8a0daa0f8c24c5469718a6a68196b4363d1c7
Youssef <youssefamr217@gmail.com>
2026-09-08 20:25:29 +0300

    Update browser title and favicon

--- api.Tests/obj/Debug/net10.0/api.Tests.AssemblyInfo.cs
+++ api.Tests/obj/Debug/net10.0/api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb25eaf1100e9368fb24a5fe083a1e5a4ed13b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
